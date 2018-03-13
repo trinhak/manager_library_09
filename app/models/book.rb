1a4913fdb7110c575455a0207c8d889ed07d495f
trinh <minhtrinhnguyendtu@gmail.com>
2018-03-13 10:59:38 +0700

    manage user

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :category
   belongs_to :author
-  
+
   has_many :like, dependent: :destroy
   has_many :borrows_detail, dependent: :destroy
   has_many :book_follow, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_many :author_follow
   has_many :like, dependent: :destroy
   has_many :borrow, dependent: :destroy
   has_many :book_follow, dependent: :destroy
   has_many :rating, dependent: :destroy
   has_many :comment, dependent: :destroy
+  validates :email, length: {maximum: Settings.user.email_length},
+    format: {with: VALID_EMAIL_REGEX}, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.user.password_length}
+  validates :name, presence: true, length: {maximum: Settings.user.name_length}
+
+  has_secure_password
 end
